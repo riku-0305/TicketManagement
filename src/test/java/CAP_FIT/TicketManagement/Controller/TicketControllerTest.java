ef6ad58fe81ba998c0e70aed9fb7e894a6fa0952
riku-0305 <rikuy03@icloud.com>
2025-09-01 01:59:16 +0900

    チケットコントローラーの会員と回数券情報を全て検索できる機能のテストを実装。

---
+++ src/test/java/CAP_FIT/TicketManagement/Controller/TicketControllerTest.java
@@ -0,0 +1,31 @@
+package CAP_FIT.TicketManagement.Controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import CAP_FIT.TicketManagement.Service.ConverterService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(TicketController.class)
+class TicketControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockitoBean
+  private ConverterService converterService;
+
+  @Test
+  void 会員と回数券の情報が全て取得できる() throws Exception {
+    mockMvc.perform(get("/userList"))
+        .andExpect(status().isOk());
+
+    Mockito.verify(converterService, Mockito.times(1)).userInfoList();
+  }
+
+}
