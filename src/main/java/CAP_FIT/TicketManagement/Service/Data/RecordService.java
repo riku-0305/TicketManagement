ebe6ad439662f82b3ae96cb6bfba3f3c4aa4e99c
riku-0305 <rikuy03@icloud.com>
2025-10-17 15:33:36 +0900

    ユーザーのカルテ情報をユーザーidを指定して検索できる機能を実装。

--- src/main/java/CAP_FIT/TicketManagement/Service/Data/RecordService.java
+++ src/main/java/CAP_FIT/TicketManagement/Service/Data/RecordService.java
@@ -1,7 +1,9 @@
 package CAP_FIT.TicketManagement.Service.Data;
 
 import CAP_FIT.TicketManagement.Data.TrainingRecord;
+import CAP_FIT.TicketManagement.Exception.UserNotFoundException;
 import CAP_FIT.TicketManagement.Repository.RecordRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +18,25 @@ public class RecordService {
   }
 
   /**
-   * 新規カルテ情報の登録が可能でrecordRepositoryからinsertRecordメソッドを呼び出す
+   * recordRepositoryからinsertRecordメソッドを呼び出す
+   * 新規カルテ情報の登録が可能
    * @param trainingRecord 新規カルテ情報
    */
   public void searchInsertRecord(TrainingRecord trainingRecord) {
     recordRepository.insertRecord(trainingRecord);
+  }
+
+  /**
+   * recordRepositoryからselectRecordListを呼び出す
+   * ユーザーidで指定されたユーザーのカルテ情報リストの取得が可能
+   * @param userId ユーザーid
+   * @return ユーザーidで指定されたユーザーのカルテ情報リスト
+   */
+  public List<TrainingRecord> selectRecordList(String userId) {
+    List<TrainingRecord> selectRecordList = recordRepository.selectRecordList(userId);
+
+    if(selectRecordList.isEmpty()) {
+      throw new UserNotFoundException("ユーザー番号　" + userId + " は存在しません");
+    } return selectRecordList;
   }
 }
